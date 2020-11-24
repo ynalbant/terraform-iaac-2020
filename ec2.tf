5738d33d0784dc9a797e738b4ee53f2338065b3e
yunus nalbant <yunusenalbant@gmail.com>
2020-11-24 22:47:21 +0000

    instances decrased to 1 and instance type changed to t2.micro

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-    count = "10"
+    count = "1"
   ami           = "ami-04bf6dcdc9ab498ca"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
