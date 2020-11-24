a6060369c5aad8b0a3aa70125556278ad75823a4
yunus nalbant <yunusenalbant@gmail.com>
2020-11-24 23:42:37 +0000

    added another resource in ec2 for import an instance

--- ec2.tf
+++ ec2.tf
@@ -8,3 +8,15 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_instance" "web2" {
+    count = "1"
+  ami           = "ami-04bf6dcdc9ab498ca"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.deployer.key_name}"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
