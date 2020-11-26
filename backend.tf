e86373d9b2d371ff64d3328e0edf0b85392932ef
yunus nalbant <yunusenalbant@gmail.com>
2020-11-26 21:40:48 +0000

    disabled dynamodb lock

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "terraform-iaac-2020-state"
     key    = "path/to/my/key"
     region = "us-east-1"
-    dynamodb_table = "terraform-iaac-2020"
+    #dynamodb_table = "terraform-iaac-2020"
   }
 }
