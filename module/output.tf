2f9262c79c70b6c9d833cd95455c62af5b65ee9b
yunus nalbant <yunusenalbant@gmail.com>
2020-11-27 12:19:01 +0000

    outputs are created

--- module/output.tf
+++ module/output.tf
@@ -5,3 +5,19 @@ output "IP" {
 output "KEY" {
     value = "${aws_instance.web.key_name}"
 }
+
+output "BucketName" {
+    value = "${aws_s3_bucket.b.bucket}"
+}
+
+output "SEC_GROUP" {
+    value = "${aws_security_group.allow_tls.name}"
+}
+
+output "AMI" {
+    value = "${aws_instance.web.ami}"
+}
+
+output "ROUTE53" {
+    value = "${aws_route53_record.wordpress.name}"
+}
