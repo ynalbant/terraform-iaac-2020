056cc82675ce4296ca320df39f4400523491fd06
yunus nalbant <yunusenalbant@gmail.com>
2020-11-28 13:27:10 +0000

    region changed to eu-west1

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-iaac-2020-state"
-    key    = "path/to/my/vpc"
+    key    = "path/to/my/eu-west-1"
     region = "us-east-1"
     #dynamodb_table = "terraform-iaac-2020"
   }
