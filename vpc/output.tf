afe5baa3c33a3fc03d3bb620b848e859b10f4e90
yunus nalbant <yunusenalbant@gmail.com>
2020-11-28 11:56:11 +0000

    ++

---
+++ vpc/output.tf
@@ -0,0 +1,23 @@
+output "VPC_ID" {
+    value = "${aws_vpc.dev.id}"
+}
+
+output "subnet1" {
+    value = "${aws_subnet.dev1.id}"
+}
+
+output "subnet2" {
+    value = "${aws_subnet.dev2.id}"
+}
+output "subnet3" {
+    value = "${aws_subnet.dev3.id}"
+}
+output "private subnet1" {
+    value = "${aws_subnet.dev_private1.id}"
+}
+output "private subnet2" {
+    value = "${aws_subnet.dev_private2.id}"
+}
+output "private subnet3" {
+    value = "${aws_subnet.dev_private3.id}"
+}
