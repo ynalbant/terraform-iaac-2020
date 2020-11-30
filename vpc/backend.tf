cc1ec2c372e092a482a265ac673805b31d07965f
yunus nalbant <yunusenalbant@gmail.com>
2020-11-30 12:24:34 +0000

    ++

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     bucket = "terraform-iaac-2020-state"
-    key    = "path/to/my/eu-west-1"
+    key    = "path/to/my/us-east-2"
 
-    region = "us-east-2"
+    region = "us-east-1"
     #dynamodb_table = "terraform-iaac-2020"
   }
 }
