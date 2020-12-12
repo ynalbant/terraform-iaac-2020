5aa6f847af739c318ff2d817ea5e50caa8aae314
yunus nalbant <yunusenalbant@gmail.com>
2020-12-12 20:50:49 +0000

    vpc_module.tf dited

--- vpc/vpc_module.tf
+++ vpc/vpc_module.tf
@@ -1,4 +1,15 @@
 module "dev" {
-    source = "./module"
-    
+ source = "./module"
+
+ region="${var.region}"
+
+ cidr_block="${var.cidr_block}"
+ 
+ cidr_block1_public = "${var.cidr_block1_public}"
+ cidr_block2_public = "${var.cidr_block2_public}"
+ cidr_block3_public = "${var.cidr_block3_public}"
+
+ cidr_block1_private="${var.cidr_block1_private}"
+ cidr_block2_private="${var.cidr_block2_private}"
+ cidr_block3_private="${var.cidr_block3_private}"
 }
