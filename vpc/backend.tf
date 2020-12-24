cc2b73cfcd14a35b4760d913fbe0f645bb3edc68
yunus nalbant <yunusenalbant@gmail.com>
2020-12-24 01:13:26 +0000

    ++

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-iaac-2020-state"
-key = "tower/us-east-1/tools/virginia/tower.tfstate"
+key = "tower/us-east-1/tools/ireland/tower.tfstate"
 region = "us-east-1"
   }
 }
