c78ff59cef2cee804eed408005bb973a9fecf5e6
yunus nalbant <yunusenalbant@gmail.com>
2020-12-25 00:28:16 +0000

    ec2.tf fixed

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -19,7 +19,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t3.micro"
 
   tags = {
