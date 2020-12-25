ec71756d35e24792265ebd4a36389afff5d5ea92
yunus nalbant <yunusenalbant@gmail.com>
2020-12-25 01:35:09 +0000

    +

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -35,6 +35,7 @@ resource "aws_key_pair" "provisioner" {
 resource "aws_instance" "web1" {
 ami = "${data.aws_ami.ubuntu.id}"
 instance_type = "t2.micro"
+key_name  = "${aws_key_pair.provisioner.key_name}"
 tags = {
 Name = "HelloWorld"
 }
@@ -76,6 +77,7 @@ output "centos_ami_id" {
 resource "aws_instance" "web" {
 ami = "${data.aws_ami.centos.id}"
 instance_type = "t2.micro"
+key_name  = "${aws_key_pair.provisioner.key_name}" 
 tags = {
 Name = "HelloWorld"
 }
