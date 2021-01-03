d451ad5ddfb5140d430fddc4cff61b53a6e58aa7
yunus nalbant <yunusenalbant@gmail.com>
2021-01-03 02:17:28 +0000

    fixed az zones

--- vpc/vpc_module.tf
+++ vpc/vpc_module.tf
@@ -1,9 +1,7 @@
 module "dev" {
  source = "./module"
 
- region="${var.region}"
-
- cidr_block="${var.cidr_block}"
+ 
  
  cidr_block1_public = "${var.cidr_block1_public}"
  cidr_block2_public = "${var.cidr_block2_public}"
@@ -12,4 +10,18 @@ module "dev" {
  cidr_block1_private="${var.cidr_block1_private}"
  cidr_block2_private="${var.cidr_block2_private}"
  cidr_block3_private="${var.cidr_block3_private}"
+
+
+az1           = "${var.az1}"
+az2           = "${var.az2 }"
+az3          = "${var.az3}"
+
+
+
+
+
+
+
+ region="${var.region}"
+ cidr_block="${var.cidr_block}"
 }
